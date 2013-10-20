e49d14a2dc3b6eb6df47d9a60d8a4ccb9b2869a7
Antti Keränen <detegr@gmail.com>
2013-10-20 12:25:39 +0300

    Hello world working, missing input

--- Interpreter.scala
+++ Interpreter.scala
@@ -1,45 +1,75 @@
+class BFState(val state: Array[Byte] = null, val cursor: Int = 0, val jumpPoint: Int = 0, val programCounter: Int=0) 
+{
+  val bfProgramSize=30000
+  val prog = state match {
+    case null => new Array[Byte](bfProgramSize)
+    case st   => st
+  }
+}
+
 class Interpreter(prog: String)
 {
-  val statements=collection.immutable.HashMap(
-    '<' -> (() => moveLeft),
-    '>' -> (() => moveRight),
-    '+' -> (() => increment),
-    '-' -> (() => decrement),
-    '.' -> (() => output),
-    ',' -> (() => input),
-    '[' -> (() => whileStart),
-    ']' -> (() => whileEnd)
+  private val progLen=prog.length
+  private val statements=collection.immutable.HashMap(
+    '<' -> ((st) => moveLeft(st)),
+    '>' -> ((st) => moveRight(st)),
+    '+' -> ((st) => increment(st)),
+    '-' -> ((st) => decrement(st)),
+    '.' -> ((st) => output(st)),
+    ',' -> ((st) => input(st)),
+    '[' -> ((st) => whileStart(st)),
+    ']' -> ((st) => whileEnd(st))
   )
 
-  private def moveRight = println("moveRight");
-  private def moveLeft = println("moveLeft");
-  private def increment = println("increment")
-  private def decrement = println("decrement")
-  private def output = println("output")
-  private def input = println("input")
-  private def whileStart = println("whileStart")
-  private def whileEnd = println("whileEnd")
+  private def moveRight(state: BFState) : BFState = new BFState(state.prog, state.cursor+1, state.jumpPoint, state.programCounter+1)
+  private def moveLeft(state: BFState) : BFState = new BFState(state.prog, state.cursor-1, state.jumpPoint, state.programCounter+1)
+  private def increment(state: BFState) : BFState = {
+    state.prog(state.cursor)=(state.prog(state.cursor).toByte + 1).toByte
+    return nextStatement(state)
+  }
 
-  def executeStatement(schar: Char)
-  {
-    val f=statements get schar match
-    {
-      case Some(f) => f
-      case None    => println("Invalid character: " + schar);
+  private def decrement(state: BFState) : BFState = {
+    state.prog(state.cursor)=(state.prog(state.cursor).toByte - 1).toByte
+    return nextStatement(state)
+  }
+
+  private def output(state: BFState) : BFState = {
+    print(state.prog(state.cursor).toChar)
+    return nextStatement(state)
+  }
+
+  private def input(state: BFState) : BFState =
+    // TODO: Get input
+    return nextStatement(state)
+
+  private def whileStart(state: BFState) : BFState =
+    return new BFState(state.prog, state.cursor, state.programCounter+1, state.programCounter+1)
+
+  private def whileEnd(state: BFState) : BFState =
+    return new BFState(state.prog, state.cursor, state.jumpPoint, state.prog(state.cursor) match {
+      case some if some > 0 => state.jumpPoint
+      case _                => state.programCounter+1
+    })
+
+  private def nextStatement(state: BFState) : BFState = new BFState(state.prog, state.cursor, state.jumpPoint, state.programCounter+1)
+  private def setNextStatement(state: BFState, pc: Int) : BFState = new BFState(state.prog, state.cursor, state.jumpPoint, pc)
+
+  private def executeStatement(state: BFState) : BFState =
+    state.programCounter match {
+      case `progLen` => return null
+      case _ => executeStatement(
+        statements get prog(state.programCounter) match {
+          case Some(f) => f(state)
+          case None    => nextStatement(state)
+        })
     }
-  }
 
-  def execute
-  {
-    prog foreach executeStatement
-  }
+  def execute = executeStatement(new BFState)
 }
 
 object Interpreter
 {
-  def main(args: Array[String])
-  {
-    val ip=new Interpreter("foobar")
-    ip.execute
+  def main(args: Array[String]) {
+    new Interpreter("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.").execute
   }
 }
